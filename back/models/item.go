253f53a78607a98b35c6f1e91b2ff4c20fa3cfc0
SuPuHe <mizin.a.s.2006@gmail.com>
2026-07-01 13:29:44 +0200

    feature: add new fields for db

--- back/models/item.go
+++ back/models/item.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Item struct {
-	ID          uint           `gorm:"primaryKey"`
-	Name        string         `gorm:"type:varchar(255);not null;index"`
-	Type        string         `gorm:"type:varchar(100);default:'misc'"`
-	Description string         `gorm:"type:text"`
-	Image       string         `gorm:"type:varchar(255)"`
-	Rarity      string         `gorm:"type:varchar(50);default:'common'"`
-	Attributes  datatypes.JSON `gorm:"type:jsonb"`
+	ID          string			`gorm:"primaryKey"`
+	Name        string			`gorm:"type:varchar(255);not null;index"`
+	Category    string			`gorm:"type:varchar(100);default:'misc';index"`
+	Price       int				`gorm:"not null;default:100"`
+	Description string			`gorm:"type:text"`
+	Image       string			`gorm:"type:varchar(255)"`
+	Rarity      string			`gorm:"type:varchar(50);default:'common'"`
+	Attributes  datatypes.JSON	`gorm:"type:jsonb"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
