8d530e51666f99b8cb7120b0a9234f4d463693e4
SuPuHe <mizin.a.s.2006@gmail.com>
2026-07-09 13:25:15 +0200

    fix: add missing files

---
+++ back/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/SmVynt/42trc/back/database"
+	"github.com/SmVynt/42trc/back/handlers"
+	"github.com/SmVynt/42trc/back/models"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load("../.env")
+
+	database.Connect()
+
+	err := database.DB.AutoMigrate(
+		&models.Item{},
+		&models.User{},
+		&models.UserCursus{},
+		&models.UserProject{},
+	)
+	if err != nil {
+		log.Fatal("AutoMigrate failed: ", err)
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Server works!")
+	})
+
+	api := r.Group("/api")
+	{
+		api.GET("/users/levels", handlers.GetLevels(database.DB))
+		api.POST("/auth/oauth/42/callback", handlers.Handle42Callback(database.DB))
+		api.GET("/auth/me", handlers.GetMe(database.DB))
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5055"
+	}
+	r.Run(":" + port)
+}
